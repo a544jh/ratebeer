f829e9060e5202cadf70b372193041bb3eace6db
a544jh <axel.wikstrom@gmail.com>
2015-01-30 23:34:34 +0200

    task10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	include RatingAverage
+	has_secure_password
 	validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+	validates :password, format: { with: /(?=.*[0-9])(?=.*[A-Z]).+/,
+									message: "has to contain at least on number and upper-case letter" },
+						 length: { minimum: 4 }
+						
 	has_many :ratings   # käyttäjällä on monta ratingia
+	has_many :beers, through: :ratings
 end
