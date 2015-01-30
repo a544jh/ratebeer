3b10d2fa70140f89dcca173d65cfd85f150f63a4
a544jh <axel.wikstrom@gmail.com>
2015-01-30 22:33:12 +0200

    task5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
 	include RatingAverage
+	validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
 	has_many :ratings   # käyttäjällä on monta ratingia
 end
