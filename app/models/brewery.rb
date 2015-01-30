c8dbfa8710f07d8730d63096aed6229000c5a35b
a544jh <axel.wikstrom@gmail.com>
2015-01-30 22:45:22 +0200

    task6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,10 +1,16 @@
 class Brewery < ActiveRecord::Base
 	include RatingAverage
+	
+	def cannot_be_in_future
+		if year.present? && year > Time.now.year
+			errors.add(:year, "can't be in the future")
+		end
+	end
 
 	has_many :beers, dependent: :destroy
 	has_many :ratings, through: :beers
 	validates :name, presence: true
 	validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                    less_than_or_equal_to: 2015,
                                     only_integer: true }
+    validate :cannot_be_in_future
 end
